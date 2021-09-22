6a6ca8c1cc031478cea612e49aa062982cc041ae
Jean Henrique <jean.jaques@ufms.br>
2021-09-22 17:44:07 -0400

    add calculo do tempo

--- Main.java
+++ Main.java
@@ -4,23 +4,32 @@ import java.util.Random;
 public class Main
 {
     public static void main (String[] args){
+
         //Cria um vetor que será ordenado
-        int[] vetor = new int[50];
+        int[] vetor = new int[1000];
         
         Random gerador = new Random();
         
         //Preenche o vetor com números inteiros aleatórios dentro de 0 ao tamanho do vetor
         for(int i=0; i<vetor.length; i++){
-            vetor[i] = gerador.nextInt(vetor.length);
+            vetor[i] = gerador.nextInt();
         }
         
         //Imprime o vetor antes da ordenação
         System.out.println(Arrays.toString(vetor));
         
         //Ordena utilizando Bubblesort
-        int[] vetorOrdenado = Ordenacao.bublesort(vetor);
+        long tempoInicial = System.nanoTime(); 
+        int[] vetorOrdenado = Ordenacao.bubleSort(vetor);
+        long tempoFinal = System.nanoTime();
+
 
         //Imprime o vetor após a ordenação
         System.out.println(Arrays.toString(vetorOrdenado));
+
+        double tempo = (double)(tempoFinal - tempoInicial)/1000000000;
+        String tempoFormatado = String.format("%.6f", tempo);
+
+        System.out.println(resultado);
     }
 }
--- Ordenacao.java
+++ Ordenacao.java
@@ -50,5 +50,32 @@ public class Ordenacao{
 	    }
 	    return vetor;
 	}	
+
+	public static int[] radixSort(int vector[]){
+		for(int digit = 0; digit < 3; digit ++){
+			int power = (int) Math.pow(10, digit+1);
+			
+			int z[][] = new int[vector.length][10];
+			int n[] = new int[10];
+
+			for(int i = 0; i < vector.length; i ++){
+				int num = vector[i];
+				z[n[(num%power)/(power/10)]][(num%power)/(power/10)] = num;
+				n[(num%power)/(power/10)]++;
+				
+			}
+			int c = 0;
+			for(int i = 0; i < 10; i ++){
+				
+				for(int j = 0; j < vector.length; j ++){
+					if(j < n[i]){
+						vector[c] = z[j][i];
+						c++;
+					}else{break;}
+				}
+			}
+		}
+		return vector;
+	}
   
 }
