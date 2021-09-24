55d2f296450599b8103a506dd91e3d7e312008a3
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2021-09-24 16:24:23 -0400

    Update Ordenacao.java

--- Ordenacao.java
+++ Ordenacao.java
@@ -1,4 +1,4 @@
-import java.util.Stack;
+import java.util.*;
 
 public class Ordenacao {
 
@@ -130,41 +130,20 @@ public class Ordenacao {
     }
 
     //radixSort
-    public static int[] radixSort(int[] arr) {
-        if (arr.length == 0)
-            return arr;
+    public static int[] bucketSort(int[] arr) {
+        int i, j;
+        int[] bucket = new int[arr.length*5];
+        Arrays.fill(bucket, 0);
 
-        int[][] np = new int[arr.length][2];
-        int[] q = new int[0x100];
-        int i, j, k, l, f = 0;
+        for (i = 0; i < arr.length; i++) {
+            bucket[arr[i]]++;
+        }
 
-        for (k = 0; k < 4; k++) {
-            for (i = 0; i < (np.length - 1); i++)
-                np[i][1] = i + 1;
-            np[i][1] = -1;
-
-            for (i = 0; i < q.length; i++)
-                q[i] = -1;
-            for (f = i = 0; i < arr.length; i++) {
-                j = ((0xFF << (k << 3)) & arr[i]) >> (k << 3);
-                if (q[j] == -1)
-                    l = q[j] = f;
-
-                else {
-                    l = q[j];
-                    while (np[l][1] != -1)
-                        l = np[l][1];
-                    np[l][1] = f;
-                    l = np[l][1];
-                }
-
-                f = np[f][1];
-                np[l][0] = arr[i];
-                np[l][1] = -1;
+        int k=0;
+        for (i = 0; i < bucket.length; i++) {
+            for (j = 0; j<bucket[i]; j++) {
+                arr[k++] = i;
             }
-            for (l = q[i = j = 0]; i < 0x100; i++)
-                for (l = q[i]; l != -1; l = np[l][1])
-                    arr[j++] = np[l][0];
         }
         return arr;
     }
